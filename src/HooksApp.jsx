f31410776f1caecea96db8a773279c7d44743ac5
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-12 11:04:26 -0300

    estilo

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 function HooksApp() {
-  return <div>HooksApp</div>;
+  return <h1>HooksApp</h1>;
 }
 
 export default HooksApp;
