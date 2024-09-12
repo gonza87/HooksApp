5a52fb1ab2277af4f4d6d344800b43942516a337
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-12 12:14:04 -0300

    counter

---
+++ src/assets/01-usestate/CounterApp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CounterApp = () => {
+  return (
+    <>
+      <h1>counter</h1>
+      <hr />
+      <button>+1</button>
+      <button>-1</button>
+    </>
+  );
+};
+
+export default CounterApp;
