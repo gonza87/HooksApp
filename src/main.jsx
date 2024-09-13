9bbe0eae24d2f068d2ff6154b87aa8d6369b09d7
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-13 12:33:30 -0300

    cambie carpeta

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import HooksApp from "./HooksApp";
 import "./index.css";
-import CounterApp from "./assets/01-usestate/CounterApp";
+import CounterApp from "./01-usestate/CounterApp";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
