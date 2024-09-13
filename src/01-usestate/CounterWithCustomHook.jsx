58b8dc25a5696f57564de3bf9e30a224b315e047
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-13 12:41:22 -0300

    btn

--- src/01-usestate/CounterWithCustomHook.jsx
+++ src/01-usestate/CounterWithCustomHook.jsx
@@ -5,6 +5,8 @@ const CounterWithCustomHook = () =>
        <h1>Counter with Hook: xxx</h1>
        <hr />
        <button className="btn btn-primary">+1</button>
+       <button className="btn btn-primary">-1</button>
+       <button className="btn btn-danger">Reset</button>
        </> 
     )
     
