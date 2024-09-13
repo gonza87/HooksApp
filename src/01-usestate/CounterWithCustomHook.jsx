0d6d248ae83bf3daa813aa1ae3e69a1f3931afba
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-13 13:00:46 -0300

    counter

--- src/01-usestate/CounterWithCustomHook.jsx
+++ src/01-usestate/CounterWithCustomHook.jsx
@@ -1,8 +1,10 @@
-const CounterWithCustomHook = () =>
-    {
+import { useCounter } from "../hooks/useCounter";
+
+const CounterWithCustomHook = () =>{
+    const { counter } = useCounter();
     return (
        <>
-       <h1>Counter with Hook: xxx</h1>
+       <h1>Counter with Hook:{counter}</h1>
        <hr />
        <button className="btn btn-primary">+1</button>
        <button className="btn btn-primary">-1</button>
