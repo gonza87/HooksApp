9cb1de5d132bbf9530ded6f4e61089d54bfd0199
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-13 12:39:55 -0300

    custom

--- src/01-usestate/CounterWithCustomHook.jsx
+++ src/01-usestate/CounterWithCustomHook.jsx
@@ -1,6 +1,13 @@
 const CounterWithCustomHook = () =>
     {
-    return <h1>custom</h1>
+    return (
+       <>
+       <h1>Counter with Hook: xxx</h1>
+       <hr />
+       <button className="btn btn-primary">+1</button>
+       </> 
+    )
+    
 }
 
 export default CounterWithCustomHook;
