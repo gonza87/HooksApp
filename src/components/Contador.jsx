dfb2e015c321314f3e62e5d33b1a54b5c32df2c5
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-20 15:37:14 -0300

    sumar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -3,11 +3,14 @@ import "./Contador.css";
 
 const Contador = () => {
     const [count, setCount] = useState(0);
+    const handleAdd = () => {
+        setCount(prevState => prevState +1)
+    }
     return(
         <div className="mt-5">
             <h2>Contador</h2>
             <p className="numero">{count}</p>
-            <button className="btn btn-primary">Sumar</button>
+            <button className="btn btn-primary" onClick={handleAdd}>Sumar</button>
             <button className="btn btn-danger">Restar</button>
             <button className="btn btn-dark">Reset</button>
         </div>
