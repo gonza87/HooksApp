f7e5f14439dadd85652da2b2749f0dfae7ac3631
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-20 15:27:58 -0300

    estilos btn

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -5,8 +5,9 @@ const Contador = () => {
         <div>
             <h2>Contador</h2>
             <p>number</p>
-            <button>Sumar</button>
-            <button>Restar</button>
+            <button className="btn btn-primary">Sumar</button>
+            <button className="btn btn-danger">Restar</button>
+            <button className="btn btn-dark">Reset</button>
         </div>
         
     )
