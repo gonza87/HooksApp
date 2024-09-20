7893a4349cbd11d39a8b9f2aef1f6ca1ed0896e1
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-20 15:33:40 -0300

    setCount

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import "./Contador.css";
 
 const Contador = () => {
+    const [count, setCount] = useState(0);
     return(
         <div className="mt-5">
             <h2>Contador</h2>
-            <p className="numero">0</p>
+            <p className="numero">{count}</p>
             <button className="btn btn-primary">Sumar</button>
             <button className="btn btn-danger">Restar</button>
             <button className="btn btn-dark">Reset</button>
