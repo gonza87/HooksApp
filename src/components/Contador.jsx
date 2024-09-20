8fdcda45090b56c8c6441651e7724c540c849267
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-20 15:31:47 -0300

    estyles

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import "./Contador.css";
 
 const Contador = () => {
     return(
-        <div>
+        <div className="mt-5">
             <h2>Contador</h2>
-            <p>number</p>
+            <p className="numero">0</p>
             <button className="btn btn-primary">Sumar</button>
             <button className="btn btn-danger">Restar</button>
             <button className="btn btn-dark">Reset</button>
