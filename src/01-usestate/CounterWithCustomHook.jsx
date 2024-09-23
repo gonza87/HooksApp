f752e5ff08ace19d7272256581eb8d0d3e1c8e07
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-23 10:58:50 -0300

    decrement

--- src/01-usestate/CounterWithCustomHook.jsx
+++ src/01-usestate/CounterWithCustomHook.jsx
@@ -1,18 +1,17 @@
 import { useCounter } from "../hooks/useCounter";
 
 const CounterWithCustomHook = () => {
-  const { counter, setCounter } = useCounter();
+  const { counter, increment, decrement } = useCounter();
   return (
     <>
       <h1>Counter with Hook:{counter}</h1>
       <hr />
-      <button
-        className="btn btn-primary"
-        onClick={() => setCounter(counter + 1)}
-      >
+      <button className="btn btn-primary" onClick={increment}>
         +1
       </button>
-      <button className="btn btn-primary">-1</button>
+      <button className="btn btn-primary" onClick={decrement}>
+        -1
+      </button>
       <button className="btn btn-danger">Reset</button>
     </>
   );
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,8 +1,18 @@
 import { useState } from "react"
 export const useCounter = (initialValue = 10) => {
     const [counter, setCounter] = useState(initialValue);
+
+    const increment = () => {
+        setCounter(prev => prev + 1);
+    }
+
+    const decrement = () => {
+        if(counter >= 1) {setCounter(prev => prev -1)}
+    }
+
     return{
         counter,
-        setCounter
+        increment,
+        decrement
     }
 }
