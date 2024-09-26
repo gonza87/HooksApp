a54e8041487da3b463b9e39c5ce28e2b966852fd
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-26 15:01:59 -0300

    component layou

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,14 @@ import { createRoot } from "react-dom/client";
 // import FormWithCustomHook from "./02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHook } from "./03-examples/MultipleCustomHook";
 
-import FocusScreen from "./04-useRef/FocusScreen";
+// import FocusScreen from "./04-useRef/FocusScreen";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Layout } from "./05-useLayoutEffect/Layout";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <FocusScreen />
+    <Layout />
+    {/* <FocusScreen /> */}
     {/* <MultipleCustomHook /> */}
     {/* <SimpleForm /> */}
     {/* <CounterApp /> */}
