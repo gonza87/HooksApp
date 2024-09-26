b0c08256e550e0d94e3436b438f59e65ef5dd04b
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-26 11:23:30 -0300

    useLayout

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useFetch } from "../hooks/useFetch";
+import { useCounter } from "../hooks/useCounter";
+import LoadingMessage from "./LoadingMessage";
+import Pokemoncard from "./Pokemoncard";
+
+export const Layout = () => {
+  const { counter, increment, decrement } = useCounter(1);
+  const { data, hasError, isLoading } = useFetch(
+    `https://pokeapi.co/api/v2/pokemon/${counter}`
+  );
+
+  return (
+    <>
+      <h1>Informacion de pokemon</h1>
+      <hr />
+      {isLoading ? (
+        <LoadingMessage />
+      ) : (
+        <Pokemoncard
+          id={data.id}
+          name={data.name}
+          sprites={[
+            data.sprites.front_default,
+            data.sprites.front_shiny,
+            data.sprites.back_default,
+            data.sprites.back_shiny,
+          ]}
+        />
+      )}
+
+      <button onClick={() => decrement()} className="btn btn-primary mt-2 me-2">
+        Anteriores
+      </button>
+      <button onClick={() => increment()} className="btn btn-primary mt-2">
+        Siguiente
+      </button>
+    </>
+  );
+};
