852d44c5d5cd9bc380e127ee4eaf8d2ef2a6b183
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-27 10:25:17 -0300

    shopw

--- src/06-memos/Memorize.jsx
+++ src/06-memos/Memorize.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useCounter } from "../hooks/useCounter";
 import { Small } from "./Small";
+import { useState } from "react";
 
 export const Memorize = () => {
   const { counter, increment } = useCounter(10);
+  const [show, setShow] = useState(true);
 
   return (
     <>
@@ -14,6 +16,8 @@ export const Memorize = () => {
       <button className="btn btn-primary" onClick={() => increment()}>
         +1
       </button>
+
+      <button>Sow/Hide {JSON.stringify(show)}</button>
     </>
   );
 };
--- src/06-memos/Small.jsx
+++ src/06-memos/Small.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export const Small = ({ value }) => {
+  console.log("Me volvi a dibujar :(");
+
   return <small>{value}</small>;
 };
