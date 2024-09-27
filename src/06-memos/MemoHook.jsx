b9425f100f9d3d86dab3831f5fec6c9ce29f7a19
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-27 10:49:40 -0300

    memocambios

--- src/06-memos/MemoHook.jsx
+++ src/06-memos/MemoHook.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useCounter } from "../hooks/useCounter";
 import { useState } from "react";
+import { useMemo } from "react";
 
 const heavyStuff = (iterationNumber = 100) => {
   for (let i = 0; i < iterationNumber; i++) {
@@ -10,8 +11,10 @@ const heavyStuff = (iterationNumber = 100) => {
 };
 
 export const MemoHook = () => {
-  const { counter, increment } = useCounter(10);
+  const { counter, increment } = useCounter(4000);
   const [show, setShow] = useState(true);
+
+  const memorizedValue = useMemo(() => heavyStuff(counter), [counter]);
 
   return (
     <>
@@ -19,7 +22,7 @@ export const MemoHook = () => {
         counter: <small>{counter}</small>
       </h1>
       <hr />
-      <h4>{heavyStuff(500)}</h4>
+      <h4>{memorizedValue}</h4>
       <button className="btn btn-primary" onClick={() => increment()}>
         +1
       </button>
