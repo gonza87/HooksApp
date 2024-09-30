7a5743e255792c0017b555eee8199fedfca69ba9
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-30 09:04:57 -0300

    counter

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 export default function CallbackHook() {
-  const [counter, setCounter] = useState(10);
+  const [counter, setCounter] = useState(1);
   return (
     <>
       <h1>usecallback Hook: {counter}</h1>
