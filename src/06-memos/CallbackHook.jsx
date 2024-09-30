d0018a3e0db2ecdc7ff29bdc3160cfb8b2c75d1a
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-09-30 09:34:03 -0300

    btnincrement

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { useState } from "react";
+import ShowIncrement from "./ShowIncrement";
 
 export default function CallbackHook() {
-  const [counter, setCounter] = useState(1);
+  const [counter, setCounter] = useState(10);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <>
       <h1>usecallback Hook: {counter}</h1>
+      <hr />
+
+      <ShowIncrement increment={increment} />
     </>
   );
 }
