aa9538267cc2a40da867dac5c19e112d5e5734b6
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-10-02 09:23:14 -0300

    tareamemo

--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,22 @@ import { createRoot } from "react-dom/client";
 
 // import CounterApp from "./01-usestate/CounterApp";
 // import CounterWithCustomHook from "./01-usestate/CounterWithCustomHook";
-
 // import SimpleForm from "./02-useEffect/SimpleForm";
 // import Contador from "./components/Contador";
-
 // import FormWithCustomHook from "./02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHook } from "./03-examples/MultipleCustomHook";
-
 // import FocusScreen from "./04-useRef/FocusScreen";
-
 // import { Layout } from "./05-useLayoutEffect/Layout";
 // import { Memorize } from "./06-memos/Memorize";
-
 // import { MemoHook } from "./06-memos/MemoHook";
-
-import CallbackHook from "./06-memos/CallbackHook";
+// import CallbackHook from "./06-memos/CallbackHook";
+import { Padre } from "./07-tarea-memo/Padre";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CallbackHook />
+    <Padre />
+    {/* <CallbackHook /> */}
     {/* <MemoHook /> */}
     {/* <Memorize /> */}
     {/* <Layout /> */}
