954d2c5d3da1d8adda61fc78e3944ce7bc5411f4
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-10-08 09:47:57 -0300

    reducer

--- src/08-useReducer/intro.reducer.js
+++ src/08-useReducer/intro.reducer.js
@@ -2,4 +2,10 @@ const initialState = [{
     id: 1,
     todo: "Recolectar la piedra del Alma",
     done: false,
-}]
+}];
+
+const todoReducer = (state = initialState, action) =>{
+
+    return state;
+
+}
