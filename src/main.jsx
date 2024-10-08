dad5dc00993d8c5af22c8a3f274f4811873df947
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-10-08 09:43:22 -0300

    cambio reducer

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,8 @@ import { createRoot } from "react-dom/client";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import CallbackHook from "./06-memos/CallbackHook";
 import { Padre } from "./07-tarea-memo/Padre";
+
+import "./08-useReducer/intro.reducer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
