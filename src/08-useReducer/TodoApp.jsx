695c24ba0f4aeafa3cd606dc350d7a6a4f36597c
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-10-15 09:23:30 -0300

    todoapp

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -4,6 +4,12 @@ function TodoApp() {
   return (
     <>
       <h1>Todo App</h1>
+      <hr />
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item3</li>
+      </ul>
     </>
   );
 }
