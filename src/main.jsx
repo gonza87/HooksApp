5f6277b2848afe5f892b079d351cf19b8caeb2ba
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-10-15 09:24:31 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -12,14 +12,16 @@ import { createRoot } from "react-dom/client";
 // import { Memorize } from "./06-memos/Memorize";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import CallbackHook from "./06-memos/CallbackHook";
-import { Padre } from "./07-tarea-memo/Padre";
+// import { Padre } from "./07-tarea-memo/Padre";
 
-import "./08-useReducer/intro.reducer";
+// import "./08-useReducer/intro.reducer";
+import TodoApp from "./08-useReducer/TodoApp";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Padre />
+    <TodoApp />
+    {/* <Padre /> */}
     {/* <CallbackHook /> */}
     {/* <MemoHook /> */}
     {/* <Memorize /> */}
