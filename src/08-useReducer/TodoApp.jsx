d54758ba9b3811daca88396d39bca23cb3879fec
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-10-17 11:09:25 -0300

    cambios totoapp

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -26,9 +26,9 @@ function TodoApp() {
       <div className="row">
         <div className="col-7">
           <ul className="list-group">
-            <li className="list-group-item">
-              <span>item 1</span>
-              <button className="btn btn-primary">Borrar</button>
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">item 1</span>
+              <button className="btn btn-danger ">Borrar</button>
             </li>
           </ul>
         </div>
