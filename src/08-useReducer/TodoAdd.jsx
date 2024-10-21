e4561efa8ae5b0bd56fa498cdaac696fa905223c
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-10-21 15:12:27 -0300

    form

---
+++ src/08-useReducer/TodoAdd.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TodoAdd = () => {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="¿Que hay que hacer?"
+        className="form-control"
+      />
+
+      <button type="submit" className="btn btn-outline-primary mt-1">
+        Agregar
+      </button>
+    </form>
+  );
+};
+
+export default TodoAdd;
--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -33,17 +33,6 @@ function TodoApp() {
         <div className="col-5">
           <h4>Agregar Todo</h4>
           <hr />
-          <form>
-            <input
-              type="text"
-              placeholder="¿Que hay que hacer?"
-              className="form-control"
-            />
-
-            <button type="submit" className="btn btn-outline-primary mt-1">
-              Agregar
-            </button>
-          </form>
         </div>
       </div>
     </>
