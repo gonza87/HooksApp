e8b15a6ad51baebcc9a73e48b07b1678d4f27351
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-10-25 17:17:15 -0300

    loginto

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -3,11 +3,13 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
 import { AboutPage } from "./AboutPage";
-
+import { Link } from "react-router-dom";
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
+      <Link to="/">Inicio</Link>
+      <Link to="/login">Login</Link>
       <hr />
       <Routes>
         <Route path="/" element={<HomePage />} />
