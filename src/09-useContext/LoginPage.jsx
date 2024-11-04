117e378b1e65e7848c0ab5ee2f348415aa83798b
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-11-04 16:14:07 -0300

    usercontext

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Usercontext } from "./context/UserContext";
 
 export const LoginPage = () => {
+  const algo = useContext(Usercontext);
+
   return (
     <>
       <h1>LoginPage</h1>
